acc6589bbbfc1cda3d7e1527168dadfff1d7ac42
Mubinjon Akilzhanov <mubinjon9009@mail.ru>
2023-04-24 14:28:32 +0700

    Debugged and started lab2

---
+++ lab2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+class lab1 {
+    int main() {
+        double a, b, c, d;
+        return 0;
+    }
+
+};
---
+++ lab2.h
@@ -0,0 +1,14 @@
+//
+// Created by MUBINJON AKILZHONOV on 24.04.2023.
+//
+
+#ifndef UNTITLED1_LAB2_H
+#define UNTITLED1_LAB2_H
+
+
+class lab2 {
+
+};
+
+
+#endif //UNTITLED1_LAB2_H
