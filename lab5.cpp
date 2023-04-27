a7c5ec6bd8096d1fb2dd378e8f949f914d2b38c3
Mubinjon Akilzhanov <mubinjon9009@mail.ru>
2023-04-27 20:16:27 +0700

    made lab5

---
+++ lab5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class lab4 {
+    int main() {
+        cout<<"Input n: ";
+        int n;
+        cin >>n;
+        if (n<=25){
+            cout<< true;
+        }
+        cout<< false;
+        return 0;
+    }
+
+};
