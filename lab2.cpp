2a7d572d5473f9ba9cb466f2a6c15e39633ff429
Mubinjon Akilzhanov <mubinjon9009@mail.ru>
2023-04-27 19:32:21 +0700

    made lab2

--- lab2.cpp
+++ lab2.cpp
@@ -1,9 +1,38 @@
-#include <iostream>
+#include "iostream"
+#include "cmath"
 using namespace std;
-class lab1 {
+class lab2 {
+    double square(double x,double y,double z){
+        double p = (x + y + z)/2;
+        return sqrt(p * (p - x) * (p - y) * (p - z));
+    }
     int main() {
-        double a, b, c, d;
+        double a, b, c, d,s;
+        while (true){
+            cout<<"Input a (1<a<2.5): ";
+            cin>>a;
+            if(a>1&&a<3){
+                break;
+            }
+        }
+        while (true){
+            cout<<"Input b (0.5<b<4.5): ";
+            cin>>b;
+            if(b>0.5&&b<4.5){
+                break;
+            }
+        }
+        while (true){
+            cout<<"(c+d>2.5)\nInput c:";
+            cin>>c;
+            cout<<"Input d:";
+            cin>>d;
+            if(a>1&&a<2.5){
+                break;
+            }
+        }
+        s= square(1,2,a)+ square(2,2.5,b)+(2.5,c,d);
+        cout<<s;
         return 0;
     }
-
 };
