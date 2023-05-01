ca54b70d8930d6601b17ab9965110ed632b7db01
Mubinjon Akilzhanov <mubinjon9009@mail.ru>
2023-05-01 12:21:17 +0700

    made lab 10(not optimized)

---
+++ lab10.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+class lab8 {
+    int main(){
+        int n;
+        cout<<"Input n: ";
+        cin>>n;
+        string m_a="",m_b="",m_c="";
+        int a[n][n];
+        int b[n][n];
+        int c[n][n];
+        int ab[n][n];
+        int ans[n][n];
+        for (int i(0);i<n;i++){
+            for (int j(0);j<n;j++){
+                a[i][j]=0 + rand() % (9 - 0 + 1);
+                b[i][j]=0 + rand() % (9 - 0 + 1);
+                c[i][j]=0 + rand() % (9 - 0 + 1);
+                m_a+= to_string(a[i][j])+" ";
+                m_b+= to_string(b[i][j])+" ";
+                m_c+= to_string(c[i][j])+" ";
+            }
+            m_a+="\n";
+            m_b+="\n";
+            m_c+="\n";
+        }
+        cout<<"Матрица А:\n"<<m_a;
+        cout<<"Матрица B:\n"<<m_b;
+        cout<<"Матрица C:\n"<<m_c;
+        for (int i(0);i<n;i++){
+            for (int j(0);j<n;j++){
+                ab[i][j]=a[i][j]+b[i][j];
+            }
+        }
+        cout<<"(A+B)*C:\n";
+        for (int i(0);i<n;i++){
+            for (int j(0);j<n;j++){
+                ans[i][j] = 0;
+                for (int k(0);k<n;k++){
+                    ans[i][j] += ab[i][k] * c[k][j];
+                }
+            }
+        }
+        cout<<"Answer: "<<endl;
+        for (int i(0);i<n;i++){
+            for (int j(0);j<n;j++){
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }
+};
