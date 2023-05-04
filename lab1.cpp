aa1a92b61ec3ec201699f1f79c7109874d09684f
Mubinjon Akilzhanov <mubinjon9009@mail.ru>
2023-05-05 01:31:09 +0700

    lab1 optimized

--- lab1.cpp
+++ lab1.cpp
@@ -5,18 +5,21 @@ using namespace std;
 double x[7];
 double find_px(double y) {
     double px =
-            (x[6] * pow(y, 6)) + (x[5] * pow(y, 5)) + (x[4] * pow(y, 4)) + (x[3] * pow(y, 3)) + (x[2] * pow(y, 2)) +
-            (x[1] * pow(y, 1)) + x[0];
+            (x[6] * pow(y, 6)) +
+            (x[5] * pow(y, 5)) +
+            (x[4] * pow(y, 4)) +
+            (x[3] * pow(y, 3)) +
+            (x[2] * pow(y, 2)) +
+            (x[1] * pow(y, 1)) +
+            x[0];
     return px;
 }
 int lab1::start() {
     for (int i(0); i <= 6; i++) {
-        cout << "Input x";
-        cout << i;
-        cout << ": ";
+        cout << "Input x"<< i<< ": ";
         cin >> x[i];
     }
-    double x1 = find_px(x[1] + 1) - find_px(x[1]);//переделать
+    double x1 = find_px(x[1] + 1) - find_px(x[1]);
     double x3 = find_px(x[3] + 1) - find_px(x[3]);
     double x4 = find_px(x[4] + 1) - find_px(x[4]);
     cout << "X1 = "<<x1<<endl;
