bb5c4bccda866e2ff45f74234dde450d10d9ea87
Mubinjon Akilzhanov <mubinjon9009@mail.ru>
2023-05-27 03:36:38 +0700

    remade lab1 with recursion.

--- lab1.cpp
+++ lab1.cpp
@@ -2,29 +2,32 @@
 #include <cmath>
 #include "lab1.h"
 using namespace std;
-double x[7];
-double find_px(double y) {
-    double px =
-            (x[6] * pow(y, 6)) +
-            (x[5] * pow(y, 5)) +
-            (x[4] * pow(y, 4)) +
-            (x[3] * pow(y, 3)) +
-            (x[2] * pow(y, 2)) +
-            (x[1] * pow(y, 1)) +
-            x[0];
-    return px;
+
+double find_px(double y, int n,double x[]) {
+    if (n==0){
+        return x[n];
+    }
+    else{
+        return x[n] * pow(y, n)+ find_px(y,n-1,x) ;
+    }
 }
 int lab1::start() {
-    for (int i(0); i <= 6; i++) {
-        cout << "Input x"<< i<< ": ";
-        cin >> x[i];
+    int n;
+    cout<<"Input n: ";
+    cin>>n;
+    double x[n];
+    for (int i(0); i <= n; i++) {
+        x[i]=1 + rand() % (10);;
+        cout<<"x"<<i<<" = "<<x[i]<<endl;
     }
-    double x1 = find_px(x[1] + 1) - find_px(x[1]);
-    double x3 = find_px(x[3] + 1) - find_px(x[3]);
-    double x4 = find_px(x[4] + 1) - find_px(x[4]);
-    cout << "Значение выражения p(x-1) - p(x) при x = 1: " << x1 << endl;
-    cout << "Значение выражения p(x-1) - p(x) при x = 3: " << x3 << endl;
-    cout << "Значение выражения p(x-1) - p(x) при x = 4: " << x4 << endl;
+    double x1 = find_px(x[1] + 1,1,x) - find_px(x[1],1,x);
+    double x3 = find_px(x[3] + 1,3,x) - find_px(x[3],3,x);
+    double x4 = find_px(x[4] + 1,4,x) - find_px(x[4],4,x);
+    double xn = find_px(x[n] + 1,n,x) - find_px(x[n],n,x);
+    cout << endl << "X1 = "<<x1<<endl;
+    cout << "X3 = "<<x3<<endl;
+    cout << "X4 = "<<x4<<endl;
+    cout << "Xn = "<<xn<<endl;
     return 0;
 }
 
